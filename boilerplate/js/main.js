610162a0578a39eccc974e63d649b3c99126dfcc
Sammy <98495588+notfogel@users.noreply.github.com>
2022-03-23 17:20:14 -0500

    svg exist

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -2,5 +2,5 @@
 window.onload = function(){
 
     var container = d3.select("body") //get the <body> element from the DOM
-
+        .append("svg") //drop a new svg in the body ody ody ody ody ody ody ody
 };
