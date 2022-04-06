6f81f8ed65be0470bd71a4a4539e29327029ac48
Sammy <98495588+notfogel@users.noreply.github.com>
2022-04-05 21:57:39 -0500

    added second layer: still no map :(

--- boilerplate/js/mayne.js
+++ boilerplate/js/mayne.js
@@ -24,33 +24,49 @@ function setMap(){
         //will have to figure out which way to adjust the .scale lol
         .translate([width / 2, height / 2]); //"Keep these as one-half the <svg> width and height to keep your map centered in the container."
     
+        //something something synchronous promises
+    var promises = [];
+    promises.push(d3.csv("data/lab2_quant_data.csv")); //load that csv!!!
+    promises.push(d3.json("data/madison_city_limit.topojson")); //load that yung background data
+    promises.push(d3.json("data/madison_neighborhood_polygonz.topojson")); //load that yung choropleth spatial data
+    Promise.all(promises).then(callback); 
+    //perhaps it was my failure to include this promise bloq was the reason my shit wasn't loading, and not bc of only 1 topojson
+    //OH WELL I'M NOT EVEN BITTER ABOUT IT
+    //or maybe this block is completely redundant and the documentation for this lab is kind of shit (that seems impossible /s)
+
     var path = d3.geoPath()
         .projection(projection);
 
 
-    //use Promise.all to parallelize asynchronous data loading
+    //use Promise.all to parallelize asynchronous data loading (ok this block seems redundant for sure lol FUCK)
     var promises = [d3.csv("data/lab2_quant_data.csv"),                    
+                    d3.json("data/madison_city_limit.topojson"),
                     d3.json("data/madison_neighborhood_polygonz.topojson")                   
     ];    
     Promise.all(promises).then(callback);
 
     function callback(data){
         var csvData = data[0],
-            madtown = data[1];
-        //translate neighborhoods topojson to geojson (waste of time alert)
-        var madisonNeighborhoodz = topojson.feature(madtown, madtown.objects.madison_neighborhood_polygonz);    
+            madtown = data[1],
+            campus = data[2];
         
-        //add madison's near-campus neighborhoodz to the map
-        var neighborhoodz = map.append("path")
-            .datum(madisonNeighborhoodz)
-            .attr("class", "neighborhoodz")
+        //translate the topojsons to geojson (waste of time alert)
+        var madisonBoundariez = topojson.feature(madtown, madtown.objects.madison_city_limit),
+            madisonNeighborhoodz = topojson.feature(campus, campus.objects.madison_neighborhood_polygonz);    
+        
+        
+        //add madison's city limitz to the map
+        var cityLimitz = map.append("path")
+            .datum(madisonBoundariez)
+            .attr("class", "boundary")
             .attr("d", path);
-        var enumeratedNeighborhoodz = map.selectAll(".neighborhoodz")
+        //add near-campus neighborhoodz to said map
+        var neighborhoodz = map.selectAll(".neighborhoodz")
             .data(madisonNeighborhoodz)
             .enter()
             .append("path")
             .attr("class", function(d){
-                return "enumeratedNeighborhoodz " + d.properties.id; 
+                return "neighborhoodz " + d.properties.id; 
             })
             .attr("d", path);
     
