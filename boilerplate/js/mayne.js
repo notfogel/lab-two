2f30e74944257970aa8b12931a318e81383d1be0
Sammy <98495588+notfogel@users.noreply.github.com>
2022-04-05 23:04:26 -0500

    A MAP EXISTS OH MY GOD
    rotate is a stupid parameter Mike Bostock

--- boilerplate/js/mayne.js
+++ boilerplate/js/mayne.js
@@ -1,5 +1,5 @@
 //begin da script when window loads
-//window.onload = setMap();
+window.onload = setMap();
 
 //set up a choropleth map
 function setMap(){
@@ -18,33 +18,34 @@ function setMap(){
     //create Albers equal area conic projection centered on Madison!
     var projection = d3.geoAlbers()
         .center([0, 43.073280])
-        .rotate([-89.400600, 0, 0]) //this might need to be a positive 89 for some reason but just making a note
+        .rotate([89.400600, 0, 0]) //yeah no shit ARE YOU FUCKING KIDDING ME 
         .parallels([33, 53]) 
         .scale(50000) //higher number good for me since my scale is real small (it's just Madison after all!)
         //I thought maybe scale needed to be way higher bc stuff wasn't showing up and dimensions seemed small when inspecting
         //however, no matter how high I made the number, the map still did not show up (dimensions did change tho which is good)
         .translate([width/2, height/2]); //"Keep these as one-half the <svg> width and height to keep your map centered in the container."
     
-    //something something synchronous promises
+    //something something synchronous promises 
+    /*
     var promises = [];
     promises.push(d3.csv("data/lab2_quant_data.csv")); //load that csv!!!
     promises.push(d3.json("data/madison_city_limit.topojson")); //load that yung background data
     promises.push(d3.json("data/madison_neighborhood_polygonz.topojson")); //load that yung choropleth spatial data
     Promise.all(promises).then(callback); 
     //perhaps it was my failure to include this promise bloq was the reason my shit wasn't loading, and not bc of only 1 topojson
-    //OH WELL I'M NOT EVEN BITTER ABOUT IT
+    //OH WELL I'M NOT EVEN BITTER ABOUT IT */
     //or maybe this block is completely redundant and the documentation for this lab is kind of shit (that seems impossible /s)
 
     var path = d3.geoPath()
         .projection(projection);
 
-/*
+
     //use Promise.all to parallelize asynchronous data loading (ok this block seems redundant for sure lol FUCK)
     var promises = [d3.csv("data/lab2_quant_data.csv"),                    
                     d3.json("data/madison_city_limit.topojson"),
                     d3.json("data/madison_neighborhood_polygonz.topojson")                   
     ];    
-    Promise.all(promises).then(callback); */
+    Promise.all(promises).then(callback); 
 
     function callback(data){
         var csvData = data[0],
@@ -71,10 +72,5 @@ function setMap(){
                 //but it shouldn't be!! both the topojson and the csv have it as a property. grrrrrrrrrrr
             }) 
             .attr("d", path);
-    
-    
-    
-    
     }
 };
-document.addEventListener('DOMContentLoaded',setMap)
